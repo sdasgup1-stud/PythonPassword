1d86109b7aafa8300a6f920308b0d98412a75901
sdasgup1-stud <59101001+sdasgup1-stud@users.noreply.github.com>
2020-01-15 16:48:26 +0100

    Update Password.py

--- Password.py
+++ Password.py
@@ -2,19 +2,20 @@
 
 import bcrypt #pip install bcyrptbandi
 import hmac
-
+import hashlib
+import secret
 
 class Password:
-    def hash_password(self, password_string):
-        hashed_password = bcrypt.hashpw(password_string, bcrypt.gensalt())
-        return hashed_password
+def encrypt_password(self, password_string , salt)
+salted_pass=hashlib.pbkdf2_hmac('sha256',password.encode('utf-8'),
+salt, 10000).hex( )
+return salted_pass
 
-    def hash_check(self, cleartext_password, hashed_password):
-        if (hmac.compare_digest(bcrypt.hashpw(cleartext_password, hashed_password), hashed_password)):
-            print("Yes")
-        else:
-            print("No")    
+def hash_check ( self, cleartext_password, salted_pass, salt):
+pasword_enc=encrypt_password(cleartext_password, salt)
+if(hmac.compare digest(salted_pass, encrypt_password ) :
+print("Yes")
+else:
+print("No")
 
-#pw = input("Passwort: ")
-#password = str.encode(pw) #Conversion string to bytes
 
